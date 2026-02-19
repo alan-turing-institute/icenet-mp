5e21d663898b685bbb4d264976dae0c7054ea4ff
James Robinson <james.em.robinson@gmail.com>
2026-02-19 16:08:32 +0000

    :alien: Specify the ImageIO FFMPEG backend which should install the appropriate platform-specific binary

--- icenet_mp/plugins.py
+++ icenet_mp/plugins.py
@@ -2,6 +2,7 @@ from logging import getLogger
 
 from icenet_mp.data_processors.filters import register_filters
 from icenet_mp.data_processors.sources import register_sources
+from icenet_mp.visualisations import register_animation_backends
 from icenet_mp.xpu import register_accelerators
 
 logger = getLogger(__name__)
@@ -11,5 +12,6 @@ def register_plugins() -> None:
     """Register all plugins."""
     logger.debug("Registering plugins with external libraries...")
     register_accelerators()
+    register_animation_backends()
     register_filters()
     register_sources()
--- icenet_mp/visualisations/__init__.py
+++ icenet_mp/visualisations/__init__.py
@@ -1,5 +1,14 @@
+from imageio_ffmpeg import get_ffmpeg_exe
+from matplotlib import rcParams
+
 from .helpers import DEFAULT_SIC_SPEC
 from .plotter import Plotter
+
+
+def register_animation_backends() -> None:
+    """Register the ImageIO FFMPEG animation backend."""
+    rcParams["animation.ffmpeg_path"] = get_ffmpeg_exe()
+
 
 __all__ = [
     "DEFAULT_SIC_SPEC",
