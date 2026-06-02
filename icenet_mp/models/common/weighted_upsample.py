767e4c47b6c7065619817f301fd5f9a763b7885a
James Robinson <james.em.robinson@gmail.com>
2026-06-02 10:39:30 +0100

    :alembic: Use two convolutions in WeightedUpsample

--- icenet_mp/models/common/weighted_upsample.py
+++ icenet_mp/models/common/weighted_upsample.py
@@ -1,3 +1,5 @@
+import math
+
 import torch
 from torch import Tensor, nn
 from torch.nn.init import kaiming_normal_
@@ -22,12 +24,16 @@ class WeightedUpsample(nn.Module):
         """
         super().__init__()
 
+        # Set the number of channels in the PixelShuffle operation such that the input
+        # channels is approximately their geometric mean.
+        hidden_channels = math.ceil(channels / upsample_factor)
+
         # Initial convolution to produce the required channels for PixelShuffle
         # We apply a kernel of the same size as the upsampling factor to allow spatial
         # mixing at the same scale.
         initial_conv = nn.Conv2d(
             channels,
-            channels * upsample_factor**2,
+            hidden_channels * upsample_factor**2,
             kernel_size=upsample_factor,
             padding="same",
         )
@@ -36,7 +42,7 @@ class WeightedUpsample(nn.Module):
         # Set all sub-channel groups to a common Kaiming-normal kernel so the initial
         # state for PixelShuffle looks like nearest-neighbour upsampling.
         group_weights = initial_conv.weight.new_empty(
-            channels, *initial_conv.weight.shape[1:]
+            hidden_channels, *initial_conv.weight.shape[1:]
         )
         kaiming_normal_(group_weights)
         with torch.no_grad():
@@ -45,10 +51,14 @@ class WeightedUpsample(nn.Module):
             )
 
         self.block = nn.Sequential(
-            # Convolution: [N, C, H, W] -> [N, C * r^2, H, W]
+            # Convolution: [N, C, H, W] -> [N, C_hidden * r^2, H, W]
             initial_conv,
-            # PixelShuffle: [N, C * r^2, H, W] -> [N, C, H * r, W * r]
+            # PixelShuffle: [N, C_hidden * r^2, H, W] -> [N, C_hidden, H * r, W * r]
             nn.PixelShuffle(upsample_factor),
+            # Convolution: [N, C_hidden, H * r, W * r] -> [N, C, H * r, W * r]
+            nn.Conv2d(
+                hidden_channels, channels, kernel_size=upsample_factor, padding="same"
+            ),
         )
 
     def forward(self, x: Tensor) -> Tensor:
