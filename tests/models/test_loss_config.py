deb3789db95b8ab869016882477965727e4a27f8
marianovitasari20 <mnovitasari@turing.ac.uk>
2026-06-03 09:12:47 +0000

    :bento: Add test loss config 3 (Initialising with a loss function that doesn't exist fails)

--- tests/models/test_loss_config.py
+++ tests/models/test_loss_config.py
@@ -41,15 +41,36 @@ class TestLossConfig:
                 # loss intentionally omitted
             )
 
-        # 2. Initialising with loss X creates a model that uses loss X
-        @pytest.mark.parametrize("loss_name", list(LOSS_CONFIGS.keys()))
-        @staticmethod
-        def test_loss_type(
-            loss_name: str,
-            cfg_input_space: DictConfig,
-            cfg_output_space: DictConfig,
-        ) -> None:
-            model = FakeDataModel(
+    # 2. Initialising with loss X creates a model that uses loss X
+    @pytest.mark.parametrize("loss_name", list(LOSS_CONFIGS.keys()))
+    @staticmethod
+    def test_loss_type(
+        loss_name: str,
+        cfg_input_space: DictConfig,
+        cfg_output_space: DictConfig,
+    ) -> None:
+        model = FakeDataModel(
+            name="fake data",
+            input_spaces=[cfg_input_space],
+            n_forecast_steps=1,
+            n_history_steps=1,
+            output_space=cfg_output_space,
+            optimizer=DictConfig({}),
+            scheduler=DictConfig({}),
+            loss=LOSS_CONFIGS[loss_name],
+        )
+        assert isinstance(model._loss_fn, LOSS_TYPES[loss_name])
+
+    # 3. Initialising with a loss function that doesn't exist fails
+    def test_nonexistent_loss_raises(
+        self, cfg_input_space: DictConfig, cfg_output_space: DictConfig
+    ) -> None:
+        bad_loss = OmegaConf.create(
+            {"_target_": "icenet_mp.losses.does_not_exist.FakeLoss"}
+        )
+        # AFTER
+        with pytest.raises(ImportError):
+            FakeDataModel(
                 name="fake data",
                 input_spaces=[cfg_input_space],
                 n_forecast_steps=1,
@@ -57,6 +78,5 @@ class TestLossConfig:
                 output_space=cfg_output_space,
                 optimizer=DictConfig({}),
                 scheduler=DictConfig({}),
-                loss=LOSS_CONFIGS[loss_name],
+                loss=bad_loss,
             )
-            assert isinstance(model._loss_fn, LOSS_TYPES[loss_name])
