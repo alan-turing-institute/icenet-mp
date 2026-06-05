fdb896800a97fe7a1f586551f8d843127a2e547f
marianovitasari20 <mnovitasari@turing.ac.uk>
2026-06-05 06:49:50 +0000

    :hammer: Modify conftest to include loss

--- tests/conftest.py
+++ tests/conftest.py
@@ -175,6 +175,10 @@ def cfg_model_service() -> DictConfig:
             "model": {
                 "_target_": "MockModel",
                 "name": "mock-model",
+                "loss": {
+                    "_target_": "torch.nn.HuberLoss",
+                    "delta": 0.5,
+                },
             },
             "predict": {
                 "target": {"group_name": "mock-dataset-group-1"},
