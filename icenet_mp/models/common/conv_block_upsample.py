c9316b74bde0d3a32d285ddf8dba1afe0f2e2402
James Robinson <james.em.robinson@gmail.com>
2026-06-05 18:19:38 +0100

    :wrench: Make upsample mode configurable in ConvBlockUpsample

--- icenet_mp/models/common/conv_block_upsample.py
+++ icenet_mp/models/common/conv_block_upsample.py
@@ -11,14 +11,14 @@ from .weighted_upsample import WeightedUpsample
 class ConvBlockUpsample(nn.Module):
     """Convolutional block that doubles spatial dimensions.
 
-    (WeightedUpsample > Norm > Act) > ConvNormAct > ConvNormAct
+    (Upsample > Norm > Act) > ConvNormAct > ConvNormAct
 
     If out_channels is not specified then this will halve the number of input channels.
 
-    Reverse of ConvBlockDownsample, using upsampling to avoid checkerboarding.
+    Reverse of ConvBlockDownsample, using upsampling to increase spatial dimensions.
     """
 
-    def __init__(
+    def __init__(  # noqa: PLR0913
         self,
         in_channels: int,
         *,
@@ -27,6 +27,7 @@ class ConvBlockUpsample(nn.Module):
         n_subblocks: int = 2,
         norm_type: str = "batchnorm",
         out_channels: int | None = None,
+        upsample_mode: str = "bilinear",
     ) -> None:
         """Initialize a ConvBlockUpsample module.
 
@@ -37,15 +38,25 @@ class ConvBlockUpsample(nn.Module):
             n_subblocks: the number of ConvNormAct blocks to stack (default 2).
             norm_type: type of normalization ("groupnorm", "batchnorm", or "none").
             out_channels: the number of output channels (if None, half of in_channels).
+            upsample_mode: the method to use for upsampling ("bilinear" or "shuffle").
 
         """
         super().__init__()
 
         out_channels = in_channels // 2 if out_channels is None else out_channels
 
+        if upsample_mode not in ("bilinear", "shuffle"):
+            msg = f"Unsupported upsample_mode: {upsample_mode}"
+            raise ValueError(msg)
+
         self.model = nn.Sequential(
-            # Size increasing upsample/normalisation/activation that maintains channels
-            WeightedUpsample(in_channels, upsample_factor=2),
+            # Upsampling layer to increase spatial dimensions
+            {
+                "bilinear": nn.Upsample(
+                    scale_factor=2, mode="bilinear", align_corners=False
+                ),
+                "shuffle": WeightedUpsample(in_channels, upsample_factor=2),
+            }[upsample_mode],
             normalisation_from_name(norm_type, in_channels),
             ACTIVATION_FROM_NAME[activation](),
             *(
