51144fa3f9f2800d9db81a0e80f1a6012597cd0d
Isabel Fenton <ifenton@turing.ac.uk>
2026-07-27 10:49:06 +0100

    :memo: Add a notebook to provide worked examples of the different metrics

--- icenet_mp/metrics/__init__.py
+++ icenet_mp/metrics/__init__.py
@@ -1,6 +1,6 @@
 from .daily_metrics import MAEPerForecastDay, RMSEPerForecastDay
 from .icenet_accuracy import IceNetAccuracyPerForecastDay
-from .sie_error_abs import SeaIceExtentErrorPerForecastDay
+from .sie_error import SeaIceExtentErrorPerForecastDay
 
 __all__ = [
     "IceNetAccuracyPerForecastDay",
