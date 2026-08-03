9970c8796b0d8a5ef6350281afbdc8a7addcffe3
marianovitasari20 <mnovitasari@turing.ac.uk>
2026-08-03 15:51:05 +0100

    :umbrella: Add data space target in encode process decode

--- icenet_mp/models/encode_process_decode.py
+++ icenet_mp/models/encode_process_decode.py
@@ -98,6 +98,7 @@ class EncodeProcessDecode(BaseModel):
         self.processor: BaseProcessor = hydra.utils.instantiate(
             processor,
             data_space=combined_latent_space,
+            data_space_target=self.target_encoder.data_space_out,
             n_forecast_steps=self.n_forecast_steps,
             n_history_steps=self.n_history_steps,
         )
