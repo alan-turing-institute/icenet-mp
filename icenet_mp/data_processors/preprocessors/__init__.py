3b315366273d2607c24d8a6e5b2c2015ba7579dc
James Robinson <james.em.robinson@gmail.com>
2026-08-18 11:16:10 +0100

    Refactor data downloading (#419)
    * :truck: Rename data_processors to ingestion
    
    * :truck: Rename data_loaders to data
    
    * :sparkles: Add initial set of postprocessors
    
    * :truck: Move masking logic to postprocessors
    
    * :wrench: Add mask postprocessor to OSISAF datasets
    
    * :coffin: Dropped the vestigial generate_masks function
    
    * :recycle: Add composite pre and post processors
    
    * :wrench: Use hydra to build preprocessors and postprocessors
    
    * :recycle: Update pre and post processors for new architecture
    
    * :recycle: Replace DataDownloaderFactory
    
    * :white_check_mark: Update tests
    
    * :wrench: Updated icenet configs for new architecture
    
    * :bug: Ignore errors when trying to remove a non-existent dataset
    
    * :bug: Make CompositePreprocessor/CompositePostprocessor construction robust against a config which sets preprocessors/postprocessors to None

--- icenet_mp/data_processors/preprocessors/__init__.py
+++
@@ -1,9 +0,0 @@
-from .icenet_sic import IceNetSICPreprocessor
-from .ipreprocessor import IPreprocessor
-from .null import NullPreprocessor
-
-__all__ = [
-    "IPreprocessor",
-    "IceNetSICPreprocessor",
-    "NullPreprocessor",
-]
