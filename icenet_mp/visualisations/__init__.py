144afeda14205333d41a0205e1c8d325ea6df9b0
Sylvester Kaczmarek <16242628+sylvesterkaczmarek@users.noreply.github.com>
2026-08-21 12:51:32 +0100

    feat: export uncertainty visualisation helpers

--- icenet_mp/visualisations/__init__.py
+++ icenet_mp/visualisations/__init__.py
@@ -4,6 +4,7 @@ from matplotlib import rcParams
 from .dataset_plotting import plot_variables_static, plot_variables_video
 from .helpers import DEFAULT_SIC_SPEC
 from .plotter import Plotter
+from .uncertainty import compute_standardised_difference, plot_static_uncertainty
 
 
 def register_animation_backends() -> None:
@@ -14,6 +15,8 @@ def register_animation_backends() -> None:
 __all__ = [
     "DEFAULT_SIC_SPEC",
     "Plotter",
+    "compute_standardised_difference",
+    "plot_static_uncertainty",
     "plot_variables_static",
     "plot_variables_video",
 ]
