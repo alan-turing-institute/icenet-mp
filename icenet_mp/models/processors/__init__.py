acc9291da3326f4762d10d83047f0f318b15b3c7
Sylvester Kaczmarek <16242628+sylvesterkaczmarek@users.noreply.github.com>
2026-08-24 20:54:33 +0100

    Export mixture-of-experts processor

--- icenet_mp/models/processors/__init__.py
+++ icenet_mp/models/processors/__init__.py
@@ -1,6 +1,7 @@
 from .base_processor import BaseProcessor
 from .ddpm import DDPMProcessor
 from .gsta import GSTAProcessor
+from .mixture_of_experts import MixtureOfExpertsProcessor
 from .null import NullProcessor
 from .unet import UNetProcessor
 from .vit import VitProcessor
@@ -9,6 +10,7 @@ __all__ = [
     "BaseProcessor",
     "DDPMProcessor",
     "GSTAProcessor",
+    "MixtureOfExpertsProcessor",
     "NullProcessor",
     "UNetProcessor",
     "VitProcessor",
