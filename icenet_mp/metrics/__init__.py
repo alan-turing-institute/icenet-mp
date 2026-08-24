17c607ac52263af2ce54b2bf8c55cd3a949cf395
Sylvester Kaczmarek <16242628+sylvesterkaczmarek@users.noreply.github.com>
2026-08-24 22:54:29 +0100

    Export Fractions Skill Score metric

--- icenet_mp/metrics/__init__.py
+++ icenet_mp/metrics/__init__.py
@@ -1,11 +1,13 @@
 from .centroid_error import CentroidErrorPerForecastDay
 from .daily_metrics import MAEPerForecastDay, RMSEPerForecastDay
+from .fractions_skill_score import FractionsSkillScorePerForecastDay
 from .icenet_accuracy import IceNetAccuracy
 from .sie_error import SIEError
 from .sie_error_abs import SeaIceExtentErrorPerForecastDay
 
 __all__ = [
     "CentroidErrorPerForecastDay",
+    "FractionsSkillScorePerForecastDay",
     "IceNetAccuracy",
     "MAEPerForecastDay",
     "RMSEPerForecastDay",
