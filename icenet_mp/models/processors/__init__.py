d95384802a79799911889c2bc7229d66d036d62a
Sylvester Kaczmarek <16242628+sylvesterkaczmarek@users.noreply.github.com>
2026-08-24 16:38:12 +0100

    Export scaled DDPM processor

--- icenet_mp/models/processors/__init__.py
+++ icenet_mp/models/processors/__init__.py
@@ -2,6 +2,7 @@ from .base_processor import BaseProcessor
 from .ddpm import DDPMProcessor
 from .gsta import GSTAProcessor
 from .null import NullProcessor
+from .scaled_ddpm import ScaledDDPMProcessor
 from .unet import UNetProcessor
 from .vit import VitProcessor
 
@@ -10,6 +11,7 @@ __all__ = [
     "DDPMProcessor",
     "GSTAProcessor",
     "NullProcessor",
+    "ScaledDDPMProcessor",
     "UNetProcessor",
     "VitProcessor",
 ]
