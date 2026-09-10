26e8a0ab0dcbffb0b396f24b6c614a1b0422b70a
Sylvester Kaczmarek <16242628+sylvesterkaczmarek@users.noreply.github.com>
2026-09-10 13:00:53 +0100

    refactor: add x0 scaling to DDPM processor

--- icenet_mp/models/processors/__init__.py
+++ icenet_mp/models/processors/__init__.py
@@ -2,7 +2,6 @@ from .base_processor import BaseProcessor
 from .ddpm import DDPMProcessor
 from .gsta import GSTAProcessor
 from .null import NullProcessor
-from .scaled_ddpm import ScaledDDPMProcessor
 from .unet import UNetProcessor
 from .vit import VitProcessor
 
@@ -11,7 +10,6 @@ __all__ = [
     "DDPMProcessor",
     "GSTAProcessor",
     "NullProcessor",
-    "ScaledDDPMProcessor",
     "UNetProcessor",
     "VitProcessor",
 ]
